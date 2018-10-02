382db2784432fb0b5488acf3e3ebf5fc78a5b817
Arseni Rynkevich <rynkevich@icloud.com>
2018-10-03 02:25:42 +0300

    Add CodeFirstExample

---
+++ CodeFirstExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CodeFirstExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SchoolContext())
+            {
+                var student1 = new Student { StudentName = "Sam", Height = 180, Weight = 74.6f };
+                var student2 = new Student { StudentName = "John", Height = 174, Weight = 67.5f };
+
+                context.Students.Add(student1);
+                context.Students.Add(student2);
+                context.SaveChanges();
+
+                Console.WriteLine("Done:");
+                foreach (var student in context.Students)
+                {
+                    Console.WriteLine("{0}. Height: {1}, Weight: {2}", student.StudentName, 
+                        student.Height, student.Weight);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CodeFirstExample/SchoolContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace CodeFirstExample
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+
+        public SchoolContext() : base("SchoolDBConnection")
+        {
+        }
+    }
+}
