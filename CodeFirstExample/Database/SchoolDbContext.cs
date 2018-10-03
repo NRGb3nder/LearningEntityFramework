b5b1a3cf3d084c4b7999cc3b99c4a03b6fa7891d
Arseni Rynkevich <rynkevich@icloud.com>
2018-10-03 03:41:55 +0300

    Add entity mappings using fluent API

--- CodeFirstExample/Database/SchoolDbContext.cs
+++ CodeFirstExample/Database/SchoolDbContext.cs
@@ -11,6 +11,23 @@ namespace CodeFirstExample.Database
         public SchoolDbContext() : base("name=SchoolDBConnection")
         {
             System.Data.Entity.Database.SetInitializer(new SchoolDbInitializer());
-        } 
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("Admin");
+
+            modelBuilder.Entity<Student>().Map(m => 
+            {
+                m.Properties(p => new { p.StudentID, p.StudentName });
+                m.ToTable("StudentInfo");
+            }).Map(m =>
+            {
+                m.Properties(p => new { p.StudentID, p.Height, p.Weight, p.Photo, p.DateOfBirth });
+                m.ToTable("StudentInfoDetail");
+            });
+
+            modelBuilder.Entity<Grade>().ToTable("GradeInfo");
+        }
     }
 }
